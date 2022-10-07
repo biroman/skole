c02358efdd497950e62e76a44c8193ecfc7ff89e
biroman <eivind-buodd@hotmail.com>
2022-10-07 16:11:44 +0200

    test

--- javascript/show-all.js
+++ javascript/show-all.js
@@ -1,4 +1,4 @@
-import FootballPlayerModule from "./FootBallPlayerModule.js";
+import FootballPlayerModule from "/FootBallPlayerModule.js";
 const showAllGrid = document.querySelector("#grid-show-all");
 const showAllBtn = document.querySelector("#show-all-btn");
 const darkModeBtn = document.querySelector("#dark-mode-btn");
